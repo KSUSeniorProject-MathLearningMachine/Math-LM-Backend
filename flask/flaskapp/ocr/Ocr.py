ec352294eabf612bc1b532d944390d565d812164
Spencer Brown <spencerb@warpmail.net>
2020-09-07 22:46:10 -0400

    Remove old Tesseract OCR references

--- flask/flaskapp/ocr/Ocr.py
+++
@@ -1,7 +0,0 @@
-import pytesseract
-from PIL import Image
-
-def Ocr():
-    def image_file_to_string(file):
-        img = Image.open(file)
-        return pytesseract.image_to_string(img).encode('ascii')
