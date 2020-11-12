ad70c3303711355f1cce35e6bcf1732701c3467d
grantw99 <grantw99@gmail.com>
2020-11-12 17:09:49 -0500

    tryin somethin

--- flask/flaskapp/CharacterSegmentation.py
+++ flask/flaskapp/CharacterSegmentation.py
@@ -208,9 +208,8 @@ def image_segmentation(pil_img):
 	# -------------Thresholding Image--------------#
 	print("\n........Program Initiated.......\n")
 	#src_img = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
-	pil_img = auto_crop(pil_img)
-	cv2.imwrite(pil_img, 'img.jpg')
-	src_img = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGB2GRAY)
+	src_img = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGB2BGR)
+	cv2.imwrite(src_img, "img.jpg")
 	orig_height, orig_width = src_img.shape
 
 	print("\n Resizing Image........")
